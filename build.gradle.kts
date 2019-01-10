b579ca16161788cef25ae4c4328ff6e41e77ce80
Gabriel Konat <gabrielkonat@gmail.com>
2019-01-10 16:17:04 +0100

    Use MetaBorg Gradle config.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,19 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-  kotlin("jvm") version "1.3.11" // Stick with version 1.3.11 because the kotlin-dsl plugin in Gradle 5.1 uses that.
+  id("org.metaborg.gradle.config") version "0.4.1" // Bootstrap with previous version.
+  id("org.metaborg.gitonium") version "0.3.0"
+  kotlin("jvm") version "1.3.11" // Use version 1.3.11 for compatibility with Gradle 5.1.
   `kotlin-dsl`
   `java-gradle-plugin`
   `maven-publish`
-  id("org.metaborg.gitonium") version "0.3.0"
 }
 
-group = "org.metaborg"
-
-repositories {
-  maven(url = "http://home.gohla.nl:8091/artifactory/all/")
-}
+// TODO: configureKotlinGradlePlugin() causes compilation errors in plugin.
+//metaborgConfig {
+//  configureKotlinGradlePlugin()
+//}
 
 dependencies {
-  compile(kotlin("stdlib"))
-  // Compile-only dependencies for Gradle plugins that we need to use types from, but should still be applied by users.
+  // Compile-only dependencies for Gradle plugins that we need to use types from, but should still be applied/provided by users.
   compileOnly("org.metaborg:gitonium:0.3.0")
   compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.11")
 }
@@ -24,37 +21,11 @@ dependencies {
 kotlinDslPluginOptions {
   experimentalWarning.set(false)
 }
-tasks.withType<KotlinCompile>().all {
-  kotlinOptions.jvmTarget = "1.8"
-}
-
 gradlePlugin {
   plugins {
     create("metaborg-config") {
       id = "org.metaborg.gradle.config"
       implementationClass = "mb.gradle.config.MetaborgConfigPlugin"
-    }
-  }
-}
-
-tasks {
-  register("buildAll") {
-    dependsOn("build")
-  }
-  register("cleanAll") {
-    dependsOn("clean")
-  }
-}
-
-publishing {
-  repositories {
-    maven {
-      name = "Artifactory"
-      url = uri("http://home.gohla.nl:8091/artifactory/all/")
-      credentials {
-        username = project.findProperty("publish.repository.Artifactory.username")?.toString()
-        password = project.findProperty("publish.repository.Artifactory.password")?.toString()
-      }
     }
   }
 }
