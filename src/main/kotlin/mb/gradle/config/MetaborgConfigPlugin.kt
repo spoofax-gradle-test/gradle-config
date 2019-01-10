1f9c8235d6ded43097612185274e66b08765caa0
Gabriel Konat <gabrielkonat@gmail.com>
2019-01-10 14:30:01 +0100

    Add MetaborgConfigExtension to subprojects.

--- src/main/kotlin/mb/gradle/config/MetaborgConfigPlugin.kt
+++ src/main/kotlin/mb/gradle/config/MetaborgConfigPlugin.kt
@@ -36,32 +36,33 @@ open class MetaborgConfigExtension(private val project: Project) {
   }
 
   fun configureKotlinGradlePlugin() {
-    project.pluginManager.apply("org.jetbrains.kotlin.jvm")
-    project.configureKotlinVersion()
-    project.configureKotlinStdLib()
-    project.pluginManager.apply("java-gradle-plugin")
     project.pluginManager.apply("kotlin-dsl")
     project.pluginManager.apply("maven-publish")
-    // TODO:
-    //    kotlinDslPluginOptions {
-    //      experimentalWarning.set(false)
-    //    }
   }
 }
 
 class MetaborgConfigPlugin : Plugin<Project> {
   override fun apply(project: Project) {
-    project.configureGroup()
-    project.configureVersion() // Only root project needs to configure version, as gitonium will set versions for subprojects automatically.
-    project.configurePublishingRepositories()
-    project.tasks {
-      createCompositeBuildTask(project, "cleanAll", "clean", "Deletes the build directory for all projects in the composite build.")
-      createCompositeBuildTask(project, "checkAll", "check", "Runs all checks for all projects in the composite build.")
-      createCompositeBuildTask(project, "assembleAll", "assemble", "Assembles the outputs for all projects in the composite build.")
-      createCompositeBuildTask(project, "buildAll", "build", "Assembles and tests all projects in the composite build.")
-      createCompositeBuildTask(project, "publishAll", "publish", "Publishes all publications produced by all projects in the composite build.")
+    project.run {
+      configureGroup()
+      // Only root project needs to configure version, as gitonium will set versions for subprojects automatically.
+      configureVersion()
+      configurePublishingRepositories()
+
+      // Only root project needs composite build tasks, as these tasks depend on tasks for subprojects.
+      tasks {
+        createCompositeBuildTask(project, "cleanAll", "clean", "Deletes the build directory for all projects in the composite build.")
+        createCompositeBuildTask(project, "checkAll", "check", "Runs all checks for all projects in the composite build.")
+        createCompositeBuildTask(project, "assembleAll", "assemble", "Assembles the outputs for all projects in the composite build.")
+        createCompositeBuildTask(project, "buildAll", "build", "Assembles and tests all projects in the composite build.")
+        createCompositeBuildTask(project, "publishAll", "publish", "Publishes all publications produced by all projects in the composite build.")
+      }
+
+      extensions.add("metaborg-config", MetaborgConfigExtension(project))
+      subprojects {
+        extensions.add("metaborg-config", MetaborgConfigExtension(project))
+      }
     }
-    project.extensions.add("metaborg-config", MetaborgConfigExtension(project))
   }
 
   private fun TaskContainerScope.createCompositeBuildTask(project: Project, allName: String, name: String, description: String) {
