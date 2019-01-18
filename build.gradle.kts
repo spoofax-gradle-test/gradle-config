c84e19fdc1794214759e7dd2a60e51e449da72cf
Gabriel Konat <gabrielkonat@gmail.com>
2019-01-18 16:16:22 +0100

    Add devenv plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 }
 
 dependencies {
+  compile("org.eclipse.jgit:org.eclipse.jgit:5.2.0.201812061821-r")
   // Compile-only dependencies for Gradle plugins that we need to use types from, but should still be applied/provided by users.
   compileOnly("org.metaborg:gitonium:0.3.0")
   compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.11")
@@ -21,6 +22,12 @@ gradlePlugin {
     create("metaborg-config") {
       id = "org.metaborg.gradle.config"
       implementationClass = "mb.gradle.config.MetaborgConfigPlugin"
+    }
+  }
+  plugins {
+    create("metaborg-devenv") {
+      id = "org.metaborg.gradle.config.devenv"
+      implementationClass = "mb.gradle.config.DevenvPlugin"
     }
   }
 }
---
+++ src/main/kotlin/mb/gradle/config/DevenvPlugin.kt
@@ -0,0 +1,110 @@
+package mb.gradle.config
+
+import org.eclipse.jgit.api.CloneCommand
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.errors.RepositoryNotFoundException
+import org.eclipse.jgit.lib.Constants
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+import org.gradle.api.*
+import java.nio.file.Files
+import java.util.*
+
+class DevenvPlugin : Plugin<Project> {
+  override fun apply(project: Project) {
+    val extension = DevenvExtension(project)
+    project.extensions.add("devenv", extension)
+    project.afterEvaluate {
+      configure(this, extension)
+    }
+  }
+
+  private fun configure(project: Project, extension: DevenvExtension) {
+    project.tasks.register("updateRepos") {
+      group = "devenv"
+      doLast {
+        val projectDir = project.projectDir
+        val urlPrefix = extension.repoUrlPrefix
+          ?: throw GradleException("Cannot update all repositories of devenv; URL prefix has not been set")
+        val rootBranch = run {
+          try {
+            FileRepositoryBuilder().readEnvironment().findGitDir(projectDir).setMustExist(true).build()
+          } catch(e: RepositoryNotFoundException) {
+            throw GradleException("Cannot update repositories of devenv; cannot retrieve current branch name because no git repository was found at $projectDir", e)
+          }.use { repo ->
+            // Use repository with 'use' to close repository after use, freeing up resources.
+            val headRef = repo.exactRef(Constants.HEAD)
+              ?: throw GradleException("Cannot update repositories of devenv; cannot retrieve current branch name because repository has no HEAD")
+            if(headRef.isSymbolic) {
+              Repository.shortenRefName(headRef.target.name)
+            } else {
+              throw GradleException("Cannot update repositories of devenv; cannot retrieve current branch name because repository HEAD is not symbolic")
+            }
+          }
+        }
+        val properties = run {
+          val properties = Properties()
+          val propertiesFile = projectDir.resolve("repo.properties").toPath()
+          if(!Files.isRegularFile(propertiesFile)) {
+            throw GradleException("Cannot update repositories of devenv; property file '$propertiesFile' does not exist or is not a file")
+          }
+          Files.newInputStream(propertiesFile).buffered().use { inputStream ->
+            properties.load(inputStream)
+          }
+          properties
+        }
+        for((name, includeOverride, urlOverride, branchOverride, dirPathOverride) in extension.repos) {
+          val include = includeOverride ?: "true" == properties.getProperty("$name.include")
+          if(!include) continue
+          val url = urlOverride ?: properties.getProperty("$name.url") ?: "$urlPrefix/$name.git"
+          val branch = branchOverride ?: properties.getProperty("$name.branch") ?: rootBranch
+          val dirName = dirPathOverride ?: properties.getProperty("$name.dir") ?: name
+          val dir = projectDir.resolve(dirName)
+          // Clone or open the repository.
+          val git = if(!dir.exists()) {
+            val clone = CloneCommand()
+            clone.setDirectory(dir)
+            clone.setURI(url)
+            clone.setBranch(branch)
+            clone.setCloneSubmodules(true)
+            clone.call()
+          } else {
+            Git.open(dir)
+          }
+          val repo = git.repository
+          if(branch != repo.branch) {
+            // Check repository out to the correct branch.
+            val checkout = git.checkout()
+            checkout.setName(branch)
+            checkout.call()
+          }
+          // Pull from remote.
+          val pull = git.pull()
+          pull.setRebase(true)
+          pull.call()
+        }
+      }
+    }
+  }
+}
+
+data class Repo(val name: String, val includeOverride: Boolean?, val urlOverride: String?, val branchOverride: String?, val dirPathOverride: String?)
+
+open class DevenvExtension(private val project: Project) {
+  internal val repos = mutableListOf<Repo>()
+
+  var repoUrlPrefix: String? = null
+
+  @JvmOverloads
+  fun registerRepo(name: String, include: Boolean? = null, url: String? = null, branch: String? = null, dirPath: String? = null) {
+    repos.add(Repo(name, include, url, branch, dirPath))
+  }
+
+  fun registerCompositeBuildTask(name: String, description: String) {
+    project.tasks.register(name) {
+      this.group = "composite build"
+      this.description = description
+      this.dependsOn(project.gradle.includedBuilds.map { it.task(":$name") })
+    }
+  }
+}
