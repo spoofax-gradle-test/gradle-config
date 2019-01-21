15404eebcbd765e9e65ca0583e02b34b66f92e25
Gabriel Konat <gabrielkonat@gmail.com>
2019-01-21 15:58:19 +0100

    Update to Metaborg Gradle config 0.5.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("org.metaborg.gradle.config") version "0.4.3" // Bootstrap with previous version.
+  id("org.metaborg.gradle.config") version "0.5.0" // Bootstrap with previous version.
   id("org.metaborg.gitonium") version "0.3.0"
   kotlin("jvm") version "1.3.11" // Use version 1.3.11 for compatibility with Gradle 5.1.
   `kotlin-dsl`
